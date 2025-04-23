bd6b4a1ac1d4f054f716099c5d9c4d40dd6d6607
Sevinc0523 <kazimlisevinc23@gmail.com>
2025-04-23 23:19:48 +0400

    Update 2b_4.py

--- 2b_4.py
+++ 2b_4.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
-# Dictionary-dən Series yaratmaq
 data = {'p': 5, 'q': 10, 'r': 15}
 s2 = pd.Series(data)
 
-# 'q' indeksli elementi seçmək
 element_q = s2['q']
 
-# Nəticəni çap etmək
 print("s2['q'] =", element_q)
