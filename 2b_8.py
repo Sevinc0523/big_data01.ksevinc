24d9fcfa0fc9c07bad0d2ce9ec0c24ea7865e1f8
Sevinc0523 <kazimlisevinc23@gmail.com>
2025-04-23 23:21:02 +0400

    Update 2b_8.py

--- 2b_8.py
+++ 2b_8.py
@@ -1,12 +1,9 @@
 import pandas as pd
 
-# List-dən DataFrame yaratmaq
 data = [(1, 2), (3, 4)]
 df1 = pd.DataFrame(data, columns=['A', 'B'])
 
-# Sətir və sütun adlarını təyin etmək
 df1.index = ['r1', 'r2']
 df1.columns = ['c1', 'c2']
 
-# Nəticəni çap etmək
 print(df1)
