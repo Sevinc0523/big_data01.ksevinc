540883ddd8ed710c9375f7f883b461e2f6febd76
Sevinc0523 <kazimlisevinc23@gmail.com>
2025-04-23 23:20:07 +0400

    Update 2b_5.py

--- 2b_5.py
+++ 2b_5.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
-# s1 adlı Series-in yaradılması
 s1 = pd.Series([10, 20, 30, 40], index=['w', 'x', 'y', 'z'])
 
-# 25-dən böyük elementləri seçmək
 greater_than_25 = s1[s1 > 25]
 
-# Nəticəni çap etmək
 print("25-dən böyük elementlər:")
 print(greater_than_25)
